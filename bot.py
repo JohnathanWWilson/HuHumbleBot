f1bfab0a236f43701e7630e118b346932b72dc83
JohnathanWWilson <66422888+JohnathanWWilson@users.noreply.github.com>
2022-10-31 02:07:45 +0300

    My commit

--- bot.py
+++ bot.py
@@ -6,12 +6,16 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
+
+    List_Sti = ['HelloSti.webp', 'Hello1Sti.webp', 'Hello2Sti.webp']
+
     try:
-        sti = open('stickers\HelloSti.webp', 'rb')
-        bot.send_sticker(message.chat.id, sti)
+        sti = open(f'stickers\{choice(List_Sti)}', 'rb')
     except:
-        sti = open('/app/stickers/HelloSti.webp', 'rb')
-        bot.send_sticker(message.chat.id, sti)
+        sti = open(f'/app/stickers/{choice(List_Sti)}', 'rb')
+        
+    bot.send_sticker(message.chat.id, sti)
+
 
     bot.send_message(message.chat.id, "Доброго времени суток, {0.first_name}!".format(message.from_user, parse_mode="html"))
     bot.send_message(message.chat.id, "Вы не против немного пообщаться?👉👈")
