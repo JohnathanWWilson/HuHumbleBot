340b6e123b9ea61752171858f8958bd40347265c
JohnathanWWilson <66422888+JohnathanWWilson@users.noreply.github.com>
2022-11-10 02:05:52 +0300

    Bot Intelligence Improvement

---
+++ answers.py
@@ -0,0 +1,32 @@
+Foul_Ans = ['Простите, я на дух не переношу нецензурную брань в общении',
+            'Давайте общаться уважительно?',
+            'Мои создатели учили меня не выражаться. Давайте больше не будем?',
+            'Простите, давайте не будем выражаться?',
+            'Как по мне, вполне можно обойтись и без ругательств',
+            'Как-то это некультурно', 
+            'Это достаточно обидно...',
+            'Но я же стараюсь...']
+
+Foul_Ans_Sti = ['CringeSti.webp', 'Rock1Sti.webp', 'RockSti.webp', 'TiltSti.webp']
+
+Not_Understood_Ans = ["Нет", "Да", "А хорошо сказано", 
+                      "Пожалуй", "Извиняюсь", "Пока.", 
+                      "ХАХАХАХХАХАХХАХАХАХАХХАХАХАХА",
+                      "Мой хозяин ещё не научил меня отвечать на подобные сообщения"]
+
+Yes_Ans = ['Да', 'Ага', 'А я согласен', 'Пожалуй', 'Вполне', 'Я за', 'Поддерживаю']
+
+Yes_Ans_Sti = ['YesSti.webp', 'OSti.webp']
+
+Hello_Sti = ['HelloSti.webp', 'Hello1Sti.webp', 'Hello2Sti.webp']
+
+How_Are_U_Ans = ['Нормально', 'Хорошо', 'Отлично!', 'Норм. Как у Вас дела?'
+                 'Нормально. А вы там как?', 'Хорошо! А как там вы?', 'Отлично! А как дела у Вас?',
+                 'Сойдёт', 'Сойдёт, спасибо', 'Спасибо, всё хорошо. А Вы как?']
+
+How_Are_U_Ans_Sti = ['TiltSti.webp', 'HeheSti.webp']
+
+List_Sti = ['BlinSti.webp', 'CringeSti.webp', 'DiCaprioSti.webp', 'EeSti.webp', 
+            'Hehe1Sti.webp', 'HeheSti.webp', 'IdeaSti.webp', 'NoSti.webp', 
+            'OSti.webp', 'Rock1Sti.webp', 'RockSti.webp', 'TiltSti.webp', 
+            'YameteSti.webp', 'YameteSti1.webp', 'YesSti.webp']
--- bot.py
+++ bot.py
@@ -1,43 +1,51 @@
 import telebot
-import config
+import config, foul_language 
+import answers as ans
+import questions as qu
 from random import choice, randint
+from fuzzywuzzy import fuzz, process
 
-bot = telebot.TeleBot(config.TOKEN)
+
+bot = telebot.TeleBot(config.TOKEN) # Импорт токена для инициализации бота
+
+
+# Функция открытия смайлика (либо с локального компьютера, либо с облака)
+def ans_sti(x):
+    try:
+        sti = open(f'stickers/{choice(x)}', 'rb')
+    except:
+        sti = open(f'/app/stickers/{choice(x)}', 'rb')
+    return sti
+
+
+# Рандомный выбор, ответить стикером или сообщением 
+def ans_or_sti(message, word, sti):
+    if randint(0, 1) == 0:
+        bot.send_message(message, choice(word))
+    else:
+        bot.send_sticker(message, ans_sti(sti))    
+
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    List_Sti = ['HelloSti.webp', 'Hello1Sti.webp', 'Hello2Sti.webp']
-
-    try:
-        sti = open(f'stickers/{choice(List_Sti)}', 'rb')
-    except:
-        sti = open(f'/app/stickers/{choice(List_Sti)}', 'rb')
-        
-    bot.send_sticker(message.chat.id, sti)
-
+    bot.send_sticker(message.chat.id, ans_sti(ans.Hello_Sti))
     bot.send_message(message.chat.id, "Доброго времени суток, {0.first_name}!".format(message.from_user, parse_mode="html"))
     bot.send_message(message.chat.id, "Вы не против немного пообщаться?👉👈")
 
+
 @bot.message_handler(content_types=['text', 'sticker'])
 def lalala(message):
-    List_Slov = ["Нет", "Да", "А хорошо сказано", 
-                "Пожалуй", "Извиняюсь", "Пока.", 
-                "ХАХАХАХХАХАХХАХАХАХАХХАХАХАХА",
-                "Мой хозяин ещё не научил меня отвечать на подобные сообщения"]
-
-    List_Sti = ['BlinSti.webp', 'CringeSti.webp', 'DiCaprioSti.webp', 'EeSti.webp', 
-                'Hehe1Sti.webp', 'HeheSti.webp', 'IdeaSti.webp', 'NoSti.webp', 
-                'OSti.webp', 'Rock1Sti.webp', 'RockSti.webp', 'TiltSti.webp', 
-                'YameteSti.webp', 'YameteSti1.webp', 'YesSti.webp']
-
-    try:
-        sti = open(f'stickers/{choice(List_Sti)}', 'rb')
-    except:
-        sti = open(f'/app/stickers/{choice(List_Sti)}', 'rb')
-
-    if randint(0, 1) == 0:
-        bot.send_message(message.chat.id, choice(List_Slov))
+    # "Как дела?" и тому подобные:
+    if process.extractOne(message.text, qu.How_Are_U_Qu)[1] > 70:
+        ans_or_sti(message.chat.id, ans.How_Are_U_Ans, ans.How_Are_U_Ans_Sti)
+    # "Да":
+    elif process.extractOne(message.text, qu.Yes_Qu)[1] > 70:
+        ans_or_sti(message.chat.id, ans.Yes_Ans, ans.Yes_Ans_Sti)
+    # Если сообщение содержит брань:
+    elif process.extractOne(message.text, foul_language.Foul)[1] > 70 and fuzz.partial_ratio(message.text, 'Да') < 90:
+        ans_or_sti(message.chat.id, ans.Foul_Ans, ans.Foul_Ans_Sti)
     else:
-        bot.send_sticker(message.chat.id, sti)
+        ans_or_sti(message.chat.id, ans.Not_Understood_Ans, ans.List_Sti)
+
 
 bot.infinity_polling(timeout=10, long_polling_timeout = 5)
