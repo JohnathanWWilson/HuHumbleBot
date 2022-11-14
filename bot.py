3b546a837d60b288c9d98f4cdbfcf730735331ab
JohnathanWWilson <66422888+JohnathanWWilson@users.noreply.github.com>
2022-11-15 00:46:41 +0300

    Adding stickers and various responses

--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@ import config, dictionary
 import answers as ans
 import questions as qu
 from random import choice, randint
-from fuzzywuzzy import fuzz, process
+from fuzzywuzzy import process
 
 
 bot = telebot.TeleBot(config.TOKEN) # Импорт токена для инициализации бота
 
 
-# Функция открытия смайлика (либо с локального компьютера, либо с облака)
+# Функция открытия стикера (либо с локальной машины, либо с облака)
 def ans_sti(x):
     try:
         sti = open(f'stickers/{choice(x)}', 'rb')
@@ -17,42 +17,76 @@ def ans_sti(x):
         sti = open(f'/app/stickers/{choice(x)}', 'rb')
     return sti
 
-
-# Рандомный выбор, ответить стикером или сообщением 
-def ans_or_sti(message, word, sti):
+def half_random(message, x, xsti, y, ysti):
     if randint(0, 1) == 0:
-        bot.send_message(message, choice(word))
+        ans_or_sti(message, x, xsti)
     else:
-        bot.send_sticker(message, ans_sti(sti))    
+        ans_or_sti(message, y, ysti)
 
 
+# Случайный выбор ответа (стикером или сообщением)
+def ans_or_sti(message, word, sti):
+    selection = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    if choice(selection) in [7, 8, 9]:
+        bot.send_sticker(message, ans_sti(sti)) 
+    else:
+        bot.send_message(message, choice(word))  
+
+# Тест вывода всех смайликов
+@bot.message_handler(commands=['stickers'])
+def demonstration(message):
+    for i in ans.List_Ans_Sti:
+        try:
+            sti = open(f'stickers/{i}', 'rb')
+        except:
+            sti = open(f'/app/stickers/{i}', 'rb')
+        bot.send_sticker(message.chat.id, sti)
+
+# Приветствие
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    bot.send_sticker(message.chat.id, ans_sti(ans.Hello_Sti))
+    bot.send_sticker(message.chat.id, ans_sti(ans.Hello_Ans_Sti))
     bot.send_message(message.chat.id, "Доброго времени суток, {0.first_name}!".format(message.from_user, parse_mode="html"))
     bot.send_message(message.chat.id, "Вы не против немного пообщаться?👉👈")
 
-
+# Основная часть интеллекта бота
 @bot.message_handler(content_types=['text', 'sticker'])
 def lalala(message):
     resemblance = process.extractOne(message.text, dictionary.Lang)
-    if resemblance[1] > 80:
-        if resemblance[0] in qu.Yes_Qu:
-            ans_or_sti(message.chat.id, ans.Yes_Ans, ans.Yes_Ans_Sti)
-        elif resemblance[0] in qu.How_Are_U_Qu:
-            ans_or_sti(message.chat.id, ans.How_Are_U_Ans, ans.How_Are_U_Ans_Sti)
-        elif resemblance[0] in qu.Who_Are_U_Qu:
-            bot.send_message(message.chat.id, choice(ans.Who_Are_U_Ans))
-        elif resemblance[0] in qu.Psychologist_Qu:
-            bot.send_message(message.chat.id, choice(ans.Psychologist_Ans))
-        elif resemblance[0] in qu.How_Old_Are_U_Qu:
-            bot.send_message(message.chat.id, choice(ans.How_Old_Are_U_Ans))
-        elif resemblance[0] in qu.What_Qu:
-            bot.send_message(message.chat.id, choice(ans.What_Ans))
-        elif resemblance[0] in qu.What_Are_U_Doing_Qu:
-            bot.send_message(message.chat.id, choice(ans.What_Are_U_Doing_Ans))
+    if message.text.isupper() and resemblance[0] not in qu.HaHa_Qu:
+        bot.send_message(message.chat.id, choice(ans.Is_Upper_Ans))
     else:
-        ans_or_sti(message.chat.id, ans.Not_Understood_Ans, ans.List_Sti)
+        if resemblance[1] > 80:
+            if resemblance[0] in qu.Yes_Qu:
+                half_random(message.chat.id, ans.Yes_Ans, ans.Yes_Ans_Sti, ans.No_Ans, ans.No_Ans_Sti)
+            elif resemblance[0] in qu.No_Qu:
+                half_random(message.chat.id, ans.No_Ans, ans.No_Ans_Sti, ans.Yes_Ans, ans.Yes_Ans_Sti)
+            elif resemblance[0] in qu.How_Are_U_Qu:
+                ans_or_sti(message.chat.id, ans.How_Are_U_Ans, ans.How_Are_U_Ans_Sti)
+            elif resemblance[0] in qu.Who_Are_U_Qu:
+                bot.send_message(message.chat.id, choice(ans.Who_Are_U_Ans))
+            elif resemblance[0] in qu.Psychologist_Qu:
+                bot.send_message(message.chat.id, choice(ans.Psychologist_Ans))
+            elif resemblance[0] in qu.How_Old_Are_U_Qu:
+                bot.send_message(message.chat.id, choice(ans.How_Old_Are_U_Ans))
+            elif resemblance[0] in qu.What_Qu:
+                ans_or_sti(message.chat.id, ans.What_Ans, ans.What_Ans_Sti)
+            elif resemblance[0] in qu.About_What_Qu:
+                ans_or_sti(message.chat.id, ans.About_What_Ans, ans.About_What_Ans_Sti)
+            elif resemblance[0] in qu.What_Are_U_Doing_Qu:
+                bot.send_message(message.chat.id, choice(ans.What_Are_U_Doing_Ans))
+            elif resemblance[0] in qu.Hello_Qu:
+                ans_or_sti(message.chat.id, ans.Hello_Ans, ans.Hello_Ans_Sti)
+            elif resemblance[0] in dictionary.Foul:
+                ans_or_sti(message.chat.id, ans.Foul_Ans, ans.Foul_Ans_Sti)
+            elif resemblance[0] in qu.Good_Night_Qu:
+                bot.send_message(message.chat.id, choice(ans.Bye_Ans))
+            elif resemblance[0] in qu.Bye_Qu:
+                bot.send_message(message.chat.id, choice(ans.Bye_Ans))
+            elif resemblance[0] in qu.HaHa_Qu:
+                ans_or_sti(message.chat.id, ans.HaHa_Ans, ans.HaHa_Ans_Sti)
+        else:
+            ans_or_sti(message.chat.id, ans.Not_Understood_Ans, ans.List_Ans_Sti)
 
 
 bot.infinity_polling(timeout=10, long_polling_timeout = 5)
