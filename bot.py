ed0ba7636cb38c4b2eab092a92289e33f0254b40
JohnathanWWilson <66422888+JohnathanWWilson@users.noreply.github.com>
2022-11-29 21:44:38 +0300

    Implemented log system

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import answers as ans
 import questions as qu
 from random import choice, randint
 from fuzzywuzzy import process
+from datetime import datetime
 
 
 bot = telebot.TeleBot(config.TOKEN) # Импорт токена для инициализации бота
@@ -12,9 +13,12 @@ bot = telebot.TeleBot(config.TOKEN) # Импорт токена для иниц�
 # Функция открытия стикера (либо с локальной машины, либо с облака)
 def ans_sti(x):
     try:
-        sti = open(f'stickers/{choice(x)}', 'rb')
+        choiced = choice(x)
+        sti = open(f'stickers/{choiced}', 'rb')
     except:
-        sti = open(f'/app/stickers/{choice(x)}', 'rb')
+        choiced = choice(x)
+        sti = open(f'/app/stickers/{choiced}', 'rb')
+    print(f'Sticker: {choiced}')
     return sti
 
 def half_random(message, x, xsti, y, ysti):
@@ -23,14 +27,26 @@ def half_random(message, x, xsti, y, ysti):
     else:
         ans_or_sti(message, y, ysti)
 
+def send_ans(x, y):
+    mesg = bot.send_message(x, y)
+    print(f'Answer: "{mesg.text}"')
 
 # Случайный выбор ответа (стикером или сообщением)
 def ans_or_sti(message, word, sti):
     selection = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     if choice(selection) in [7, 8, 9]:
-        bot.send_sticker(message, ans_sti(sti)) 
+        bot.send_sticker(message, ans_sti(sti))
     else:
-        bot.send_message(message, choice(word))  
+        send_ans(message, choice(word))
+
+# Логи
+def espionage(message):
+    print(f'\n{datetime.now()}')
+    print(f'Username: {message.from_user.username}')
+    print(f'First name: {message.from_user.first_name}')
+    print(f'Last name: {message.from_user.last_name}')
+    print(f'id: {message.from_user.id}')
+    print(f'Message: "{message.text}"')
 
 # Тест вывода всех смайликов
 @bot.message_handler(commands=['stickers'])
@@ -45,16 +61,19 @@ def demonstration(message):
 # Приветствие
 @bot.message_handler(commands=['start'])
 def welcome(message):
+    print()
     bot.send_sticker(message.chat.id, ans_sti(ans.Hello_Ans_Sti))
-    bot.send_message(message.chat.id, "Доброго времени суток, {0.first_name}!".format(message.from_user, parse_mode="html"))
-    bot.send_message(message.chat.id, "Вы не против немного пообщаться?👉👈")
+    send_ans(message.chat.id, "Доброго времени суток, {0.first_name}!".format(message.from_user, parse_mode="html"))
+    send_ans(message.chat.id, "Вы не против немного пообщаться?👉👈")
 
 # Основная часть интеллекта бота
 @bot.message_handler(content_types=['text', 'sticker'])
 def lalala(message):
+    espionage(message)
     resemblance = process.extractOne(message.text, dictionary.Lang)
+    print(f'Comparison: {resemblance}')
     if message.text.isupper() and resemblance[0] not in qu.HaHa_Qu:
-        bot.send_message(message.chat.id, choice(ans.Is_Upper_Ans))
+        send_ans(message.chat.id, choice(ans.Is_Upper_Ans))
     else:
         if resemblance[1] > 80:
             if resemblance[0] in qu.Yes_Qu:
@@ -64,17 +83,17 @@ def lalala(message):
             elif resemblance[0] in qu.How_Are_U_Qu:
                 ans_or_sti(message.chat.id, ans.How_Are_U_Ans, ans.How_Are_U_Ans_Sti)
             elif resemblance[0] in qu.Who_Are_U_Qu:
-                bot.send_message(message.chat.id, choice(ans.Who_Are_U_Ans))
+                send_ans(message.chat.id, choice(ans.Who_Are_U_Ans))
             elif resemblance[0] in qu.Psychologist_Qu:
-                bot.send_message(message.chat.id, choice(ans.Psychologist_Ans))
+                send_ans(message.chat.id, choice(ans.Psychologist_Ans))
             elif resemblance[0] in qu.How_Old_Are_U_Qu:
-                bot.send_message(message.chat.id, choice(ans.How_Old_Are_U_Ans))
+                send_ans(message.chat.id, choice(ans.How_Old_Are_U_Ans))
             elif resemblance[0] in qu.What_Qu:
                 ans_or_sti(message.chat.id, ans.What_Ans, ans.What_Ans_Sti)
             elif resemblance[0] in qu.About_What_Qu:
                 ans_or_sti(message.chat.id, ans.About_What_Ans, ans.About_What_Ans_Sti)
             elif resemblance[0] in qu.What_Are_U_Doing_Qu:
-                bot.send_message(message.chat.id, choice(ans.What_Are_U_Doing_Ans))
+                send_ans(message.chat.id, choice(ans.What_Are_U_Doing_Ans))
             elif resemblance[0] in qu.Hello_Qu:
                 ans_or_sti(message.chat.id, ans.Hello_Ans, ans.Hello_Ans_Sti)
             elif resemblance[0] in dictionary.Foul:
@@ -82,11 +101,11 @@ def lalala(message):
             elif resemblance[0] in qu.Good_Night_Qu:
                 ans_or_sti(message.chat.id, ans.Good_Night_Ans, ans.Good_Night_Ans_Sti)
             elif resemblance[0] in qu.Bye_Qu:
-                bot.send_message(message.chat.id, choice(ans.Bye_Ans))
+                send_ans(message.chat.id, choice(ans.Bye_Ans))
             elif resemblance[0] in qu.HaHa_Qu:
                 ans_or_sti(message.chat.id, ans.HaHa_Ans, ans.HaHa_Ans_Sti)
             elif resemblance[0] in qu.How_Many_Qu:
-                bot.send_message(message.chat.id, choice(ans.How_Many_Ans))
+                send_ans(message.chat.id, choice(ans.How_Many_Ans))
         else:
             ans_or_sti(message.chat.id, ans.Not_Understood_Ans, ans.List_Ans_Sti)
 
